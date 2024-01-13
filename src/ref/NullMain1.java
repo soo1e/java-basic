7a068c402a61644dc3e7d696150b680cccdbacf1
soo1e <gameswuon@gmail.com>
2024-01-13 16:33:38 +0900

    sec 2

---
+++ src/ref/NullMain1.java
@@ -0,0 +1,14 @@
+package ref;
+
+public class NullMain1 {
+    public static void main(String[] args) {
+        Data data = null;
+        System.out.println("1. Data: " + data);
+
+        data = new Data();
+        System.out.println("2. Data: " + data);
+
+        data = null;
+        System.out.println("3. Data: " + data);
+    }
+}
